7ba55a72df24237b4ac32f0d7fc8aa3d9f6f4c89
Leon Tselios <90699862+LeonTselios22@users.noreply.github.com>
2024-02-01 13:19:59 +0100

    Kundendaten
    Kundendaten halt.

---
+++ Kundendaten.py
@@ -0,0 +1,78 @@
+Name = input("Gib dein richtigen Namen ein: ")
+print("Name is: " + Name)
+
+
+
+Alter = input("Wie alt biste?: ")
+print("Alter is: " + Alter)
+
+
+
+
+username = input("Gib dein User Name ein: ")
+print("Username is: " + username)
+
+
+print("\nZusammenfassung:")
+print("Name: " + Name)
+print("Alter: " + Alter)
+print("Username: " + username)
+
+
+
+f = open("Kundendaten.txt", "w")
+
+
+
+datei = open('Kundendaten.txt','a')
+
+datei.write(" \nName " + Name)
+
+datei.write(" \nAlter " + Alter)
+
+datei.write(" \nUsername " + username)
+
+#Hallo Marcel du bist cool wenn du das ließt.
+
+
+#       _v   ,,
+ #     /`|   &&.
+  #    `-\`-&&&&&.
+   #      \&&&&&&&\
+    #      &&#""&& \
+     #    / |   |\  x
+      #   \ |  / /
+
+
+ #                     _.-**-._
+ #                   _,(        ),_
+ #                .-"   '-^----'   "-.
+ #             .-'                    '-.
+ #           .'                          '.
+ #         .'    __.--**'""""""'**--.__    '.
+ #        /_.-*"'__.--**'""""""'**--.__'"*-._\
+ #       /_..-*"'   .-*"*-.  .-*"*-.   '"*-.._\
+ #      :          /       ;:       \          ;
+ #      :         :     *  !!  *     :         ;
+ #       \        '.     .'  '.     .'        /
+ #        \         '-.-'      '-.-'         /
+ #     .-*''.                              .'-.
+ #  .-'      '.                          .'    '.
+ # :           '-.        _.._        .-'        '._
+ #;"*-._          '-._  --___ `   _.-'        _.*'  '*.
+#:#      '.            `"*-.__.-*"`           (        :
+ #;      ;                 *|                 '-.     ;
+ # '---*'                   |                    ""--'
+ #  :                      *|                      :
+ #  '.                      |                     .'
+ #    '.._                 *|        ____----.._-'
+ #     \  """----_____------'-----"""         /
+ #      \  __..-------.._        ___..---._  /
+ #      :'"              '-..--''          "';
+ #       '""""""""""""""""' '"""""""""""""""'
+
+
+
+
+      
+
